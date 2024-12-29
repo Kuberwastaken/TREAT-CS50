05bc380249e71a0add34a06418b307c8fb976960
Kuberwastaken <Kuberhob@gmail.com>
2024-12-29 20:28:13 +0530

    Made the website less hardcoded

--- treat/app/__init__.py
+++ treat/app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+import os
 
-# Set the template and static folder paths
-app = Flask(__name__, 
-            template_folder='C:/Users/yokub/Documents/TREAT-Project/templates', 
-            static_folder='C:/Users/yokub/Documents/TREAT-Project/static')
+# Get the absolute path for the template and static folders
+base_dir = os.path.abspath(os.path.dirname(__file__))
+template_dir = os.path.join(base_dir, "../../templates")
+static_dir = os.path.join(base_dir, "../../static")
+
+app = Flask(
+    __name__,
+    template_folder=template_dir,
+    static_folder=static_dir
+)
 CORS(app)
 
-from . import routes
+# Import routes after initializing Flask app to avoid circular import issues
+from app import routes
